03c7bcdb3fb463b22159a6da7d7f141181ddbcea
Balázs Márton <106063573+embee-dev@users.noreply.github.com>
2024-05-13 21:34:57 +0200

    use fixed name for the TOC file, add extra comments for user path

--- pdf-merger.mjs
+++ pdf-merger.mjs
@@ -1,15 +1,15 @@
 "use strict"
 /*
 * USER PATH
-* - program starts, initiated in the current directory
-* OR
-* - program starts, uses the directory provided as a command line argument
+* + program starts, uses the directory provided as a command line argument
 * - checks if a TOC.json is available in the working directory
 * - if yes, alerts the user and skips the reading/JSON writing parts, goes straight to user prompt
-* - adds a config object to the JSON, adds "name" that defaults to the current directory
-* - reads all PDF files in the working directory (alphabetical order)
-* - stores them in an array
-* - writes the array as JSON in the working directory
+@TODO handle/read/process existing TOC.json file
+* + adds a config object to the JSON, adds "name" that defaults to the current directory
+* + reads all PDF files in the working directory (alphabetical order)
+@TODO create sort function if necessary
+* + stores them in an array
+* + writes the array as JSON in the output directory
 * - prints a message that the JSON is generated, the user must edit the file if necessary
 * - wait for user prompt, continue or exit
 * - reads the array of files from TOC.json
@@ -26,12 +26,14 @@ import { argv } from 'node:process'
 // program defaults
 const config = {
 
+    // value used as space for JSON.stringify, when generating the TOC.json file
+    jsonSpace: 4,
+
     // generated files will go in this folder
     outDir: 'generated',
 
     // default file extensions
     extensions: {
-        json: '.json',
         pdf: '.pdf'
     }
 }
@@ -39,8 +41,7 @@ const config = {
 // this object will be filled in with the appropiate data (filename, toc filename etc.)
 const tocObject = {
     // name of the Table-Of-Contents file that will be used to determine the order of files
-    // defaults to "current directory".json
-    toc: '',
+    toc: 'TOC.json',
 
     // name of the generated combined file
     // defaults to "current directory".pdf
@@ -52,7 +53,7 @@ export function start() {
     const workingDirectory = argv?.[2] ?? '.'
     const folderName = path.parse(workingDirectory).name
     
-    tocObject.toc = folderName.concat(config.extensions.json)
+    // file name will be based on the folder name of the current directory
     tocObject.output = folderName.concat(config.extensions.pdf)
 
     // reads all files in current directory and filters them (only allowed extensions according to config)
@@ -62,8 +63,11 @@ export function start() {
         path.extname(item).toLowerCase() === config.extensions.pdf
     )
 
+    // create output directory if necessary
     fs.mkdirSync(config.outDir, {recursive: true})
-    fs.writeFileSync(path.join(config.outDir, tocObject.toc), JSON.stringify({...tocObject, files}), { flag: 'w' })
+
+    // write TOC.json file
+    fs.writeFileSync(path.join(config.outDir, tocObject.toc), JSON.stringify({...tocObject, files}, null, config.jsonSpace), { flag: 'w' })
     console.log(`${tocObject.toc} written`)
 }
 
