8a9011ba3261df3461b727a6275df695b22e19f2
embee-dev <balazs.z.marton.dev@gmail.com>
2024-05-16 16:16:35 +0200

    add TODO comment for upcoming features

--- pdf-merger.mjs
+++ pdf-merger.mjs
@@ -2,9 +2,9 @@
 * USER PATH
 * + program starts, uses the directory provided as a command line argument
 * + check if working directory is available, exits if no
-* - checks if a TOC.json is available in the working directory
+* + checks if a TOC.json is available in the working directory
 * - if yes, alerts the user and skips the reading/JSON writing parts, goes straight to user prompt
-@TODO handle/read/process existing TOC.json file
+* + @TODO handle/read/process existing TOC.json file
 * + adds a config object to the JSON, adds "name" that defaults to the current directory
 * + reads all PDF files in the working directory (alphabetical order)
 @TODO create sort function if necessary
@@ -12,9 +12,9 @@
 * + writes the array as JSON in the output directory
 * - prints a message that the JSON is generated, the user must edit the file if necessary
 * - wait for user prompt, continue or exit
-* - reads the array of files from TOC.json
-* - loops through each PDF file and merges them
-* - writes the output using the "name" property in the TOC.json file
+* + reads the array of files from TOC.json
+* + loops through each PDF file and merges them
+* + writes the output using the "name" property in the TOC.json file
 */
 
 // import { PDFDocument, StandardFonts, rgb } from 'pdf-lib'
@@ -210,6 +210,9 @@ export class pdfMerger {
             }
             
         }
+
+        // @TODO ask for user input to continue from here
+        // or let the user edit the TOC file first
 
         // do real work
         // see https://github.com/Hopding/pdf-lib/issues/252#issuecomment-566063380
