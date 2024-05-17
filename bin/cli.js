7460b26e1c44e3341ee4c4e8e92d11a9812a29b6
embee-dev <balazs.z.marton.dev@gmail.com>
2024-05-17 17:12:56 +0200

    create beta use of argparse

--- bin/cli.js
+++ bin/cli.js
@@ -1,9 +1,19 @@
 #!/usr/bin/env node
 
 import { pdfMerger } from '../pdf-merger.mjs'
-import { argv } from 'node:process'
+import { argv, exit } from 'node:process'
+import { ArgumentParser } from 'argparse'
+
+const parser = new ArgumentParser({
+    description: 'PDF merger'
+});
+
+console.dir(parser.parse_args());
 
 let args = argv.slice(2)
+
+    parser.print_help()
+    exit();
 
 let myPdfMerger = new pdfMerger(args[0] ?? '.')
 myPdfMerger.start()
