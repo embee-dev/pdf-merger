5f9ce99c247cee6be6cf2fc2a295e8c350a927f0
embee-dev <balazs.z.marton.dev@gmail.com>
2024-05-22 10:49:11 +0200

    create simplified starter program

--- bin/cli.js
+++ bin/cli.js
@@ -1,15 +1,9 @@
 #!/usr/bin/env node
 
 import { pdfMerger } from '../pdf-merger.mjs'
-import { exit } from 'node:process'
-import { ArgumentParser } from 'argparse'
+import { cwd, argv } from 'node:process'
 
-const parser = new ArgumentParser({
-    description: 'PDF merger'
-})
 
-// let args = parser.parse_args()
-exit()
 
-const myPdfMerger = new pdfMerger()
-myPdfMerger.sourceDirectory = args.sourceDirectory
+const myPdfMerger = new pdfMerger(argv?.[2] ?? cwd())
+myPdfMerger.start()
