f9635ad89d549bd4f9cfe1bcc2c835dac6de8e9d
Balázs Márton <106063573+embee-dev@users.noreply.github.com>
2024-05-23 10:00:23 +0200

    create init function, add basic command line parameters

--- bin/cli.js
+++ bin/cli.js
@@ -1,9 +1,30 @@
 #!/usr/bin/env node
 
 import { pdfMerger } from '../pdf-merger.mjs'
-import { cwd, argv } from 'node:process'
+import { argv } from 'node:process'
+import { ArgumentParser } from 'argparse'
 
+const myPdfMerger = new pdfMerger()
+const commandLineParameters = argv.slice(2)
+let args
 
+const parser = new ArgumentParser({
+    description: 'PDF Merger',
+    add_help: true
+})
+parser.add_argument('-s', '--source', { help: 'Path to the source directory, defaults to current folder' })
+parser.add_argument('-o', '--output', { help: 'Name of the generated file, defaults to the name of the current folder: "current folder".pdf' })
+parser.add_argument('-i', '--interactive', { help: 'Gives the user the possibility to edit options like the name of the generated file etc.', action: 'store_true' })
 
-const myPdfMerger = new pdfMerger(argv?.[2] ?? cwd())
+if (commandLineParameters.length) {
+    args = parser.parse_args()
+} else {
+    args = {}
+}
+
+myPdfMerger.init({
+    sourceDirectory: args?.source,
+    targetFile: args?.output,
+    interactive: args.interactive
+})
 myPdfMerger.start()
