06a7c89bbd73e5be58f1be3c1f4915f398053a57
Balázs Márton <106063573+embee-dev@users.noreply.github.com>
2024-05-30 11:13:55 +0200

    create simple CLI script for first NPM release

--- bin/cli.js
+++ bin/cli.js
@@ -1,39 +1,8 @@
 #!/usr/bin/env node
 
 import { PDFMerger } from '../pdf-merger.mjs'
-import { argv } from 'node:process'
-import { ArgumentParser } from 'argparse'
-
-import { messageTypes, printMessage } from '../modules/messagePrinter.mjs'
 
 const myPdfMerger = new PDFMerger()
-const commandLineParameters = argv.slice(2)
-let args
 
-const parser = new ArgumentParser({
-    description: 'PDF Merger',
-    add_help: true
-})
-parser.add_argument('-s', '--source', {
-    help: 'Path to the source directory, defaults to current folder'
-})
-parser.add_argument('-o', '--output', {
-    help: 'Name of the generated file, defaults to the name of the current folder: "current folder".pdf'
-})
-parser.add_argument('-i', '--interactive', {
-    action: 'store_true',
-    help: 'Gives the user the possibility to edit options like the name of the generated file etc.'
-})
-
-if (commandLineParameters.length) {
-    args = parser.parse_args()
-} else {
-    args = {}
-}
-
-myPdfMerger.init({
-    sourceDirectory: args?.source,
-    targetFile: args?.output,
-    interactive: args.interactive
-})
+myPdfMerger.init()
 myPdfMerger.start()
